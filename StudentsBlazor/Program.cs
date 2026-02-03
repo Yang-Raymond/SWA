8760619b6b08f82b5f8e03bcdef3ca301d42e5a1
Raymond <raymondyang59@gmail.com>
2026-02-03 11:01:37 -0800

    changed api url

--- StudentsBlazor/Program.cs
+++ StudentsBlazor/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string apiUrl = "http://localhost:7071/api/";
+string apiUrl = "/";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
